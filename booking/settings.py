67be38cef9ee479aeeab7551a21348f6f45bdbbd
Merzaad <Merzaad@gmail.com>
2021-12-31 13:38:15 +0330

    result

--- booking/settings.py
+++ booking/settings.py
@@ -95,6 +95,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT=os.path.join(BASE_DIR,'statics')
 
 # Default primary key field type
 
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,8 @@
-from types import NoneType
 from django.shortcuts import render, redirect
 from .models import room, room_number, reservation
 from django.views import View
 from django.contrib import messages
-from django.contrib.auth.models import User
-import datetime , json
-import pytz
+import datetime 
 c=[] #Temporary to save filter query.
 c_in =''
 c_out=''
@@ -14,11 +11,9 @@ c_out=''
 #utc.localize(datetime_data)
 
 class home(View):
-    
     def get(self, request):
             return render(request,'home.html')
     def post(self, request ):
-       
         city=request.POST['destenition']
         checkin=datetime.datetime.strptime(request.POST['check_in'], '%Y-%m-%d').date()
         checkout=datetime.datetime.strptime(request.POST['check_out'], '%Y-%m-%d').date()
@@ -28,16 +23,11 @@ class home(View):
         c_out = request.POST['check_out']
         #adults=request.POST['adults']
         #children=request.POST['children']
-
-        
-        
-
         a=[]
         available_rooms = room_number.objects.filter(room__destination=city)
         for i in available_rooms:
             a.append(i.id)
 
-       
         b=[]
         not_available_reservations = reservation.objects.filter(room__id__in=a)
         for i in not_available_reservations:
@@ -47,8 +37,7 @@ class home(View):
                 messages.info(request,'suggestion check in: {}'.format(i.check_out))
             if i.check_in < checkout:
                 messages.info(request,'suggestion check out: {}'.format(i.check_in))
-       
-            
+
         global c 
         c=[]
         for i in a:
@@ -56,7 +45,6 @@ class home(View):
                 c.append(i)
 
         return redirect('/results')
-
 
 class results(View):
         def get(self, request):
@@ -72,5 +60,3 @@ class results(View):
             res = reservation ( customer = current_user , room =  res_room , check_in = datetime.datetime.strptime(c_in, '%Y-%m-%d').date() , check_out = datetime.datetime.strptime(c_out, '%Y-%m-%d').date() )
             res.save()
             return redirect('/')
-            
-
